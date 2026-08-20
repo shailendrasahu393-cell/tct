b2d093acd7f8231a808323662ec7aa491dae8f4a
shailendra sahu <shailendrasahu393@gmail.com>
2026-08-20 15:44:53 +0530

    Register Netlify Python API function

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -2,4 +2,8 @@ from mangum import Mangum
 
 from backend.firebase_app import app
 
-handler = Mangum(app)
+asgi_handler = Mangum(app)
+
+
+def handler(event, context):
+    return asgi_handler(event, context)
